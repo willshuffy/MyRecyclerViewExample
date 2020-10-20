3f67796b0ce1c9b9fb399be4ddc79d4fcb92df10
willshuffy <saputrawilly26@gmail.com>
2020-10-20 11:04:36 +0700

    RecyclerView Mode List with Adapter

--- app/src/main/java/com/willshuffyproject/myrecyclerviewexample/MainActivity.kt
+++ app/src/main/java/com/willshuffyproject/myrecyclerviewexample/MainActivity.kt
@@ -2,11 +2,43 @@ package com.willshuffyproject.myrecyclerviewexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val list = ArrayList<Hero>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rv_heroes.setHasFixedSize(true)
+
+        list.addAll(getListHeroes())
+        showRecyclerList()
+    }
+
+    private fun showRecyclerList() {
+        rv_heroes.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = ListHeroAdapter(list)
+        rv_heroes.adapter = listHeroAdapter
+    }
+
+    fun getListHeroes(): ArrayList<Hero> {
+        val dataName = resources.getStringArray(R.array.data_name)
+        val dataDescription = resources.getStringArray(R.array.data_description)
+        val dataPhoto = resources.getStringArray(R.array.data_photo)
+
+        val listHero = ArrayList<Hero>()
+        for (position in dataName.indices){
+            val hero = Hero(
+                dataName[position],
+                dataDescription[position],
+                dataPhoto[position]
+            )
+            listHero.add(hero)
+        }
+        return listHero
     }
 }
