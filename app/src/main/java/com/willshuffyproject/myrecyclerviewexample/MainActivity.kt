ebab13e96757a9f925ba5edac7031069e0a49b75
willshuffy <saputrawilly26@gmail.com>
2020-10-20 11:16:50 +0700

    Adding Option Menus

--- app/src/main/java/com/willshuffyproject/myrecyclerviewexample/MainActivity.kt
+++ app/src/main/java/com/willshuffyproject/myrecyclerviewexample/MainActivity.kt
@@ -2,6 +2,8 @@ package com.willshuffyproject.myrecyclerviewexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -40,5 +42,29 @@ class MainActivity : AppCompatActivity() {
             listHero.add(hero)
         }
         return listHero
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setMode(selectedMode: Int) {
+        when(selectedMode){
+            R.id.action_list -> {
+                showRecyclerList()
+            }
+            R.id.action_grid ->{
+
+            }
+            R.id.action_cardview -> {
+                
+            }
+        }
     }
 }
