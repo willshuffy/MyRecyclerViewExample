bbf45eb2b27ae7e74df69f21c46358c2b20e5ad9
willshuffy <saputrawilly26@gmail.com>
2020-10-20 12:50:09 +0700

    RecyclerView Mode Grid with GridAdapter

--- app/src/main/java/com/willshuffyproject/myrecyclerviewexample/MainActivity.kt
+++ app/src/main/java/com/willshuffyproject/myrecyclerviewexample/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -60,11 +61,17 @@ class MainActivity : AppCompatActivity() {
                 showRecyclerList()
             }
             R.id.action_grid ->{
-
+                showRecyclerGrid()
             }
             R.id.action_cardview -> {
-                
+
             }
         }
+    }
+
+    private fun showRecyclerGrid(){
+        rv_heroes.layoutManager = GridLayoutManager(this, 2)
+        val gridHeroAdapter = GridHeroAdapter(list)
+        rv_heroes.adapter = gridHeroAdapter
     }
 }
